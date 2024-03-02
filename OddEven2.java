28136a99a84ab097c4bceb0f158bd32d46c40e85
mohamedfahimyousuf <mohamedfahimyousuf786@gmail.com>
2024-03-02 19:08:48 +0530

    Uploaded the code

--- OddEven2.java
+++ OddEven2.java
@@ -1,1 +1,21 @@
+//To find the given number is odd or even by getting the input from the user
 
+//Using If Else 
+
+import java.io.*;
+import java.util.*;
+
+public class oddEven2 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int n= sc.nextInt();
+        if(n%2==0){
+            System.out.println("The given no is EVEN");
+        }
+        else{
+            System.out.println("The given no is ODD");
+        }
+        sc.close();
+    }
+}
