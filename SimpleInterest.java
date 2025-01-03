a6bec04e2660771264999a0cf8af06d553077cb2
mohamedfahimyousuf <mohamedfahimyousuf786@gmail.com>
2025-01-03 21:46:15 +0530

    Simple interest program

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -1,3 +1,14 @@
+import java.util.*;
 public class SimpleInterest {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Principal: ");
+        int p = scanner.nextInt();
+        System.out.print("Enter Time: ");
+        int t = scanner.nextInt();
+        System.out.print("Enter Rate: ");
+        int r = scanner.nextInt();
+        int si = (p*r*t)/100;
+        System.out.print("Simple Interest: "+ si);
+    }
 }
