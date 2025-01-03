f317180019429635adbd50b135322107995d1ec8
mohamedfahimyousuf <mohamedfahimyousuf786@gmail.com>
2025-01-03 23:18:30 +0530

    Fibonacci series problem

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+        int a = 0;
+        int b = 1;
+        System.out.println("Fibonacci series upto"+ n + " terms");
+        for(int i =1;i<=n;i++){
+            System.out.print(a + " ");
+            int next = a+b;
+            a= b;
+            b= next;
+        }
+    }
+}
