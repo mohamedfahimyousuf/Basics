ab5ef0558141cbb208c1b79317d5d9d56232709e
mohamedfahimyousuf <mohamedfahimyousuf786@gmail.com>
2025-01-03 23:04:34 +0530

    INR ti USD program

---
+++ InrToUsd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class InrToUsd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("The exchange rate is 1 USD = 83 INR");
+        int usd = 83;
+        System.out.print("Enter the INR amount: ");
+        int inr = scanner.nextInt();
+        double con = inr/usd;
+        System.out.printf("USD : %.2f $", con); 
+    }
+}
