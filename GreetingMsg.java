70a89a94a21ca286f414d962cca6e69aaaa243c8
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-02-09 22:03:53 +0530

    Update GreetingMsg.java

--- GreetingMsg.java
+++ GreetingMsg.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class GreetingMsg{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a name: ");
+        System.out.print("Enter a name : ");
         String name = scanner.nextLine();
         System.out.println("Welcome "+name);
         scanner.close();
