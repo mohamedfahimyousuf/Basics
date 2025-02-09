7a7cff08a9154dc186fdacbcf368c64022607f92
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-02-09 22:03:24 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Armstrong {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number:");
         int num = scanner.nextInt();
         int originalNum = num;
         int sum = 0;
