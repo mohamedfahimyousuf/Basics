c5c94821bcd6f46612d3b1a1673825db621d17ae
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-02-19 19:30:35 +0530

    Update OddEven1.java

--- OddEven1.java
+++ OddEven1.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class OddEven1{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a Number: ");
+        System.out.println("Enter a Number : ");
         int n= sc.nextInt();
         System.out.println(n%2==0? "The given no is EVEN":"The given no is ODD");
         sc.close();
