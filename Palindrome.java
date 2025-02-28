41a977db233f31edba3085db38ac4856e54f6738
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-02-28 17:05:10 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a Word: ");
+        System.out.print("Enter a Word : ");
         String input = scanner.nextLine();
         input = input.toLowerCase();
         String reversed = new StringBuilder(input).reverse().toString();
